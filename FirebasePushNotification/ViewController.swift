a1c8c1cee543fd16725b9bef4340157c6c98a61c
hardipkalola <hardipkalola@gmail.com>
2018-12-20 18:09:48 +0530

    Final Code

---
+++ FirebasePushNotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebasePushNotification
+//
+//  Created by Hardip Kalola on 07/12/18.
+//  Copyright © 2018 Hardip Kalola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
